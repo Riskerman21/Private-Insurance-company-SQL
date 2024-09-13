92f48556474444dc2b8c3474a9b500605ad6e8fb
Abdallah Azazy <61993263+Riskerman21@users.noreply.github.com>
2024-09-13 13:01:34 +0000

    easy

--- a4.sql
+++ a4.sql
@@ -2,5 +2,5 @@ SELECT P.PolicyID
 FROM Policy P
 JOIN Vehicle V ON V.VehicleID = P.VehicleID
 JOIN VehicleCodeMapping M ON V.VehicleCode = M.VehicleCode
-WHERE P.PolicyPurchaseDate > '2021-05-30'
+WHERE P.PolicyPurchaseDate >= '2021-05-30'
 AND M.Make LIKE "T%" OR M.Model LIKE "T%";
