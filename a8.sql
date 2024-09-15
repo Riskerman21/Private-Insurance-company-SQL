122af59b9e3f49dfc760b1f9d115a631b5c25439
Abdallah Azazy <61993263+Riskerman21@users.noreply.github.com>
2024-09-15 07:48:46 +0000

    lecuture showed me the way

--- a8.sql
+++ a8.sql
@@ -1,14 +1,16 @@
-WITH MAKES AS (
+SELECT M.Make
+FROM (
     SELECT VM.Make, AVG(V.EstYearlyKm) AS AvgDistance
     FROM Vehicle V
     JOIN VehicleCodeMapping VM ON V.VehicleCode = VM.VehicleCode
     GROUP BY VM.Make
-),
-HIGHESTAVG AS (
-    SELECT MAX(AvgDistance) AS MaxAvgDistance
-    FROM MAKES
-)
-SELECT M.Make
-FROM MAKES M
-JOIN HIGHESTAVG H
-ON M.AvgDistance = H.MaxAvgDistance;
+) AS M
+WHERE M.AvgDistance = (
+    SELECT MAX(AvgDistance)
+    FROM (
+        SELECT AVG(V.EstYearlyKm) AS AvgDistance
+        FROM Vehicle V
+        JOIN VehicleCodeMapping VM ON V.VehicleCode = VM.VehicleCode
+        GROUP BY VM.Make
+    ) AS Subquery
+);
