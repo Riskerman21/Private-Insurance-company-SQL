879749cd12fb63338b1393813021e271c662364b
Abdallah Azazy <61993263+Riskerman21@users.noreply.github.com>
2024-09-15 08:21:56 +0000

    optomisation

--- a8.sql
+++ a8.sql
@@ -1,11 +1,8 @@
-SELECT M.Make
-FROM (
-    SELECT VM.Make, AVG(V.EstYearlyKm) AS AvgDistance
-    FROM Vehicle V
-    JOIN VehicleCodeMapping VM ON V.VehicleCode = VM.VehicleCode
-    GROUP BY VM.Make
-) AS M
-WHERE M.AvgDistance = (
+SELECT VM.Make
+FROM Vehicle V
+JOIN VehicleCodeMapping VM ON V.VehicleCode = VM.VehicleCode
+GROUP BY VM.Make
+HAVING AVG(V.EstYearlyKm) = (
     SELECT MAX(AvgDistance)
     FROM (
         SELECT AVG(V.EstYearlyKm) AS AvgDistance
