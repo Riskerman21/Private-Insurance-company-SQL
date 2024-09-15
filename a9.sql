d2aec4aacb0b63245dc7242769afc56532123bdc
Abdallah Azazy <61993263+Riskerman21@users.noreply.github.com>
2024-09-15 08:24:21 +0000

    one final optamisation

--- a9.sql
+++ a9.sql
@@ -1,12 +1,8 @@
 SELECT C.CustomerID
 FROM Customer C
-JOIN (
-    SELECT P.CustomerID
-    FROM Policy P
-    JOIN Vehicle V ON P.VehicleID = V.VehicleID
-    WHERE V.VehiclePurpose = 'Business'
-    GROUP BY P.CustomerID
-    HAVING COUNT(P.VehicleID) >= 3
-) BC ON C.CustomerID = BC.CustomerID
-WHERE C.Email LIKE '%uqconnect.edu.au%' 
-   OR C.Email LIKE '%uq.edu.au%';
+JOIN Policy P ON C.CustomerID = P.CustomerID
+JOIN Vehicle V ON P.VehicleID = V.VehicleID
+WHERE V.VehiclePurpose = 'Business'
+  AND (C.Email LIKE '%uqconnect.edu.au%' OR C.Email LIKE '%uq.edu.au%')
+GROUP BY C.CustomerID
+HAVING COUNT(P.VehicleID) >= 3;
