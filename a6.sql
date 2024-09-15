8f55d30c872666ee02463d97e9582a63c6d49d19
Abdallah Azazy <61993263+Riskerman21@users.noreply.github.com>
2024-09-15 08:04:25 +0000

    thank god for the lecture

--- a6.sql
+++ a6.sql
@@ -1,13 +1,8 @@
-WITH TotalPremiums AS (
-    SELECT CustomerID, SUM(Premium) AS TotalPolicy
+SELECT CustomerID
+FROM Policy
+GROUP BY CustomerID
+HAVING SUM(Premium) > ALL (
+    SELECT SUM(Premium)
     FROM Policy
     GROUP BY CustomerID
-),
-MaxPremium AS (
-    SELECT MAX(TotalPolicy) AS MaxTotalPolicy
-    FROM TotalPremiums
-)
-SELECT TP.CustomerID
-FROM TotalPremiums TP
-JOIN MaxPremium MP
-ON TP.TotalPolicy = MP.MaxTotalPolicy;
+);
