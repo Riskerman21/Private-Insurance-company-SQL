e48320aefbe33acaa82f7cc37eaad6e104a96587
Abdallah Azazy <61993263+Riskerman21@users.noreply.github.com>
2024-09-18 08:51:01 +0000

    restrection blah blah blah

--- a9.sql
+++ a9.sql
@@ -3,6 +3,12 @@ FROM Customer C
 JOIN Policy P ON C.CustomerID = P.CustomerID
 JOIN Vehicle V ON P.VehicleID = V.VehicleID
 WHERE V.VehiclePurpose = 'Business'
-  AND (C.Email LIKE '%uqconnect.edu.au%' OR C.Email LIKE '%uq.edu.au%')
 GROUP BY C.CustomerID
-HAVING COUNT(P.VehicleID) >= 3;
+HAVING COUNT(P.VehicleID) >= 3
+
+INTERSECT
+
+SELECT C.CustomerID
+FROM Customer C
+WHERE C.Email LIKE '%uqconnect.edu.au%' 
+   OR C.Email LIKE '%uq.edu.au%';
