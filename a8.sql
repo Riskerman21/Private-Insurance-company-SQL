b42242a4f4c314144cc34d9be7186a68c1ab6508
Abdallah Azazy <61993263+Riskerman21@users.noreply.github.com>
2024-09-29 02:33:54 +0000

    veiws

--- a8.sql
+++ a8.sql
@@ -1,13 +1,13 @@
-SELECT VM.Make
+CREATE VIEW AvgYearlyKmByMake AS
+SELECT VM.Make, AVG(V.EstYearlyKm) AS AvgDistance
 FROM Vehicle V
 JOIN VehicleCodeMapping VM ON V.VehicleCode = VM.VehicleCode
-GROUP BY VM.Make
-HAVING AVG(V.EstYearlyKm) = (
-    SELECT MAX(AvgDistance)
-    FROM (
-        SELECT AVG(V.EstYearlyKm) AS AvgDistance
-        FROM Vehicle V
-        JOIN VehicleCodeMapping VM ON V.VehicleCode = VM.VehicleCode
-        GROUP BY VM.Make
-    ) AS AVG
-);
+GROUP BY VM.Make;
+
+CREATE VIEW MaxAvgDistance AS
+SELECT MAX(AvgDistance) AS MaxDistance
+FROM AvgYearlyKmByMake;
+
+SELECT Make
+FROM AvgYearlyKmByMake
+WHERE AvgDistance = (SELECT MaxDistance FROM MaxAvgDistance);
